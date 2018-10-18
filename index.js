16351a060d5726d3b0ef253d537e64e3f6006f2f
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-10-18 13:06:25 +0700

    Level 1

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+// let print=""
+// for (i=1 ; i<=5 ; i++){
+//     print=print+"*"
+//     console.log(print)
+// }
+console.log("Level 0")
 let print=""
-for (i=1 ; i<=5 ; i++){
-    print=print+"*"
-    console.log(print)
-}
-
-let print2=""
 for(j=1;j<=5;j++){
     for(k=j;k>=1;k--){
-        print2=print2+"* "
+        print=print+"* "
         }
-        console.log(print2)
-        print2=""
+        console.log(print)
+        print=""
+}
+
+console.log("")
+console.log("Level 1")
+print=""
+for(j=1;j<=5;j++){
+    for(k=j;k<=5;k++){
+        print=print+"* "
+        }
+        console.log(print)
+        print=""
 }

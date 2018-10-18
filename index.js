d57c252c4f8222e1e722ffa7dbe4072bebda6d9f
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-10-18 12:58:15 +0700

    Level 1 and 2

---
+++ index.js
@@ -0,0 +1,14 @@
+let print=""
+for (i=1 ; i<=5 ; i++){
+    print=print+"*"
+    console.log(print)
+}
+
+let print2=""
+for(j=1;j<=5;j++){
+    for(k=j;k>=1;k--){
+        print2=print2+"* "
+        }
+        console.log(print2)
+        print2=""
+}

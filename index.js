a0c924c4ebdd8ef81686a34777aaa0fe26776c31
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-10-18 16:55:42 +0700

    Level 4

--- index.js
+++ index.js
@@ -3,47 +3,65 @@
 //     print=print+"*"
 //     console.log(print)
 // }
-console.log("Level 0")
-let print=""
-for(j=1;j<=5;j++){
-    for(k=j;k>=1;k--){
-        print=print+"* "
-        }
-        console.log(print)
-        print=""
-}
+// console.log("Level 0")
+// let print=""
+// for(j=1;j<=5;j++){
+//     for(k=j;k>=1;k--){
+//         print=print+"* "
+//         }
+//         console.log(print)
+//         print=""
+// }
+
+// console.log("")
+// console.log("Level 1")
+// print=""
+// for(j=1;j<=5;j++){
+//     for(k=j;k<=5;k++){
+//         print=print+"* "
+//         }
+//         console.log(print)
+//         print=""
+// }
+
+// console.log("")
+// console.log("Level 2")
+// print=""
+// var rows = 5; 
+// for(var i=0; i<rows; i++)
+// {
+//  for(var k=1; k<=(rows-i);k++)
+// {
+//   print +=" "
+//  }
+//  for(var j=1; j<=(2*i+1); j++)
+//  {
+//   print += "*"
+//  }
+//  print += "\n"
+// }
+// console.log(print)
+
+// console.log("")
+// console.log("Level 3")
+// print=""
+// var rows = 5; 
+// for(var i=0; i<rows; i++)
+// {
+//  for(var k=1; k<=i;k++)
+// {
+//   print +=" "
+//  }
+//  print +="*** "
+//  print += 5-i
+//  print += "\n"
+// }
+// console.log(print)
+
+
 
 console.log("")
-console.log("Level 1")
-print=""
-for(j=1;j<=5;j++){
-    for(k=j;k<=5;k++){
-        print=print+"* "
-        }
-        console.log(print)
-        print=""
-}
-
-console.log("")
-console.log("Level 2")
-print=""
-var rows = 5; 
-for(var i=0; i<rows; i++)
-{
- for(var k=1; k<=(rows-i);k++)
-{
-  print +=" "
- }
- for(var j=1; j<=(2*i+1); j++)
- {
-  print += "*"
- }
- print += "\n"
-}
-console.log(print)
-
-console.log("")
-console.log("Level 3")
+console.log("Level 4")
 print=""
 var rows = 5; 
 for(var i=0; i<rows; i++)
@@ -52,8 +70,12 @@ for(var i=0; i<rows; i++)
 {
   print +=" "
  }
- print +="*** "
- print += 5-i
+ for(var j=1; j<(10-(i*2)) ; j++)
+ {
+  print += "*"
+ }
  print += "\n"
 }
 console.log(print)
+
+
